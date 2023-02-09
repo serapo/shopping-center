a0fce1900ef0e31b3f12bfb1dfed7b3f46e60b05
Serap <77828715+serapo@users.noreply.github.com>
2023-02-09 16:04:47 +0300

    ShoppingPage img updated

--- src/components/ShoppingPage.vue
+++ src/components/ShoppingPage.vue
@@ -1,46 +1,40 @@
 <template>
+  <v-container>
+    <v-col>
+    <v-row align="stretch">
+      <v-col v-for="card in shopAll" :key="card.title">
+        <v-card>
+          <v-img
+        
+            :src="card.images[0]"
+            class="mx-auto"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="250"
+            width="350"
+          >
+          </v-img>
+          <v-card-title class="text-center" v-text="card.title"></v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
 
-    <v-container > 
-      <v-row >
-        <v-col
-          v-for="card in shopAll"
-          :key="card.title"
-          :cols="card.flex"
-        >
-          <v-card >
-            <v-img
-              :src="card.images[0]"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="150"
-              width="250"
-            >
-              
-            </v-img>
-            <v-card-title v-text="card.title"></v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
 
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-bookmark</v-icon>
+            </v-btn>
 
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-
+            <v-btn icon>
+              <v-icon>mdi-share-variant</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-col>
+  </v-container>
 </template>
-
-
 
 <script lang="ts">
 import { defineComponent } from "vue";
