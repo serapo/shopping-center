3f90474922e0348f5cc1754cd0c158c242358a5d
Serap <77828715+serapo@users.noreply.github.com>
2023-02-09 14:32:42 +0300

    shopping-center

---
+++ src/navigations/ToolbarNavigations.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <v-toolbar
+      dark
+      prominent
+      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Shopping Center</v-toolbar-title>
+  
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+  </div>
+  <v-navigation-drawer v-model="drawer" app dark permanent class="grey lighten-1 mt-12">
+        <v-list>
+          <v-list-item
+            v-for="link in links"
+            :key="link.text"
+            router
+            :to="link.route"
+          >
+            <v-list-item-action>
+              <v-icon>{{ link.icon }}</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title> {{ link.text }} </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {
+          icon: "dashboard",
+          text: "Dashboard",
+          route: "/dashboard",
+          newTab: false,
+        },
+        {
+          icon: "leaderboard",
+          text: "Stats",
+          route: "https://www.google.com ",
+          newTab: true,
+        },
+        {
+          icon: "qr_code_scanner",
+          text: "QR Codes",
+          route: "/qr-codes",
+          newTab: false,
+        },
+        {
+          icon: "tungsten",
+          text: "Campaigns",
+          route: "/Campaigns",
+          newTab: false,
+        },
+        {
+          icon: "link",
+          text: "URL Groups",
+          route: "/url-groups",
+          newTab: false,
+        },
+        {
+          icon: "settings",
+          text: "Settings",
+          route: "/settings",
+          newTab: false,
+        },
+        {
+          icon: "support",
+          text: "Support",
+          route: "https://youtube.com",
+          newTab: true,
+        },
+        { icon: "exit_to_app", text: "Logout", route: "/" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
