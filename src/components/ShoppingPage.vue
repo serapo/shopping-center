47a336eb9b455417c99278771e7702774e9b2fb0
Serap <77828715+serapo@users.noreply.github.com>
2023-02-10 09:38:14 +0300

    createStore created

--- src/components/ShoppingPage.vue
+++ src/components/ShoppingPage.vue
@@ -1,45 +1,46 @@
 <template>
   <v-container>
     <v-col>
-    <v-row align="stretch">
-      <v-col v-for="card in shopAll" :key="card.title">
-        <v-card>
-          <v-img
-        
-            :src="card.images[0]"
-            class="mx-auto"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            height="250"
-            width="350"
-          >
-          </v-img>
-          <v-card-title class="text-center" v-text="card.title"></v-card-title>
-          <v-card-actions>
-            <v-spacer></v-spacer>
+      <v-row align="stretch">
+        <v-col v-for="card in items" :key="card.title">
+          <v-card>
+            <v-img
+              :src="card.images[0]"
+              class="mx-auto"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="250"
+              width="350"
+            >
+            </v-img>
+            <v-card-title
+              class="text-center"
+              v-text="card.title"
+            ></v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
 
-            <v-btn icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
 
-            <v-btn icon>
-              <v-icon>mdi-bookmark</v-icon>
-            </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
 
-            <v-btn icon>
-              <v-icon>mdi-share-variant</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-col>
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
   </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
-import axios from "axios";
+import { mapActions, mapGetters, mapMutations, mapState } from "vuex";
 
 export default defineComponent({
   name: "ShoppingPage",
@@ -50,11 +51,14 @@ export default defineComponent({
       show: false,
     };
   },
-  mounted() {
-    axios.get("https://api.escuelajs.co/api/v1/products").then((response) => {
-      this.shopAll = response.data;
-      console.log(this.shopAll, "bbbbbb");
-    });
+  computed: {
+    ...mapState(["items"]),
+  },
+  mounted(){
+    this.loadItems();
+  },
+  methods: {
+    ...mapActions(["loadItems"]),
   },
 });
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,29 @@
-import { createStore } from 'vuex'
+import axios from 'axios';
+import { createStore} from 'vuex'
+
 
 export default createStore({
   state: {
+    items: [] as Array<any>,
   },
   getters: {
+  
   },
   mutations: {
+    SET_Item (state, items) {
+      state.items = items
+  }
   },
   actions: {
+    loadItems ({ commit }) {
+      axios
+          .get('https://api.escuelajs.co/api/v1/products')
+          .then(response => response.data)
+          .then(items => {
+              console.log(items);
+          commit('SET_Item', items)
+      })
+  }
   },
   modules: {
   }
