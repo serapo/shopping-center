f6f53e8a5fe0164ca8c7691e50d95d82e91bbc51
Serap <77828715+serapo@users.noreply.github.com>
2023-03-06 11:36:26 +0300

    Clothes updated

--- src/components/Clothes.vue
+++ src/components/Clothes.vue
@@ -1,15 +1,50 @@
 <template>
-   <v-container>
-    <p>Clothes page </p>
-  </v-container>
+  <v-row>
+    <v-col sm="12" md="6" xl="3" v-for="card in clothesArray" :key="card.title">
+      <v-card width="100%">
+        <v-img
+          :src="card.images[0]"
+          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          cover
+        >
+        </v-img>
+        <v-card-title class="text-center">{{ card.title }}</v-card-title>
+        <v-col align="center">
+          <v-label class="text-h5 text-black"> ₺{{ card.price }}</v-label>
+        </v-col>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
+import { mapActions, mapState } from "vuex";
 export default {
-
-}
+  name: "Clothes",
+  data() {
+    return {
+      shopAll: [] as Array<any>,
+      show: false,
+      myIconColor: "black",
+      showNumber: false,
+      clothesArray: [] as any,
+    };
+  },
+  computed: {
+    ...mapState(["items", "count", "countBasket"]),
+  },
+  mounted() {
+    this.loadItems();
+    this.items.filter((x: any) => {
+      if (x.category.id == 1) {
+        this.clothesArray.push(x);
+      }
+    });
+  },
+  methods: {
+    ...mapActions(["loadItems"]),
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
